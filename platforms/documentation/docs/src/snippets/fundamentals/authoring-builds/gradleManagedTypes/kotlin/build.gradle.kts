137441a49eb6adc8ae346e7dee9b53a61a125825
Laura Kassovic <lkassovic@gradle.com>
2026-04-22 09:28:25 -0700

    snippetize gradle_managed_types_intermediate.adoc

---
+++ platforms/documentation/docs/src/snippets/fundamentals/authoring-builds/gradleManagedTypes/kotlin/build.gradle.kts
@@ -0,0 +1,31 @@
+// tag::eager-evaluation[]
+val version = project.version.toString()         // evaluated now
+val file = File("build/output.txt")              // evaluated now
+myTask.outputFile.set(file)
+// end::eager-evaluation[]
+
+// tag::lazy-evaluation[]
+val outputFile: RegularFileProperty = project.objects.fileProperty()
+outputFile.set(layout.buildDirectory.file("output.txt"))    // evaluated later
+// end::lazy-evaluation[]
+
+// tag::eager-task[]
+// Not Configuration Cache compatible
+tasks.register("printVersion") {
+    doLast {
+        val version = project.version.toString()
+        println("Version is $version")
+    }
+}
+// end::eager-task[]
+
+// tag::lazy-task[]
+// Configuration Cache compatible
+tasks.register("printVersion") {
+    val version: Property<String> = project.objects.property(String::class.java)
+    version.set(project.version.toString())
+    doLast {
+        println("Version is ${version.get()}")
+    }
+}
+// end::lazy-task[]
